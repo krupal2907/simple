a64b47071c98f8098df32f074eac6ec61d9f8ba3
krupal2907 <45603479+krupal2907@users.noreply.github.com>
2019-07-06 12:00:33 +0530

    Update setupwifi.sh

--- setupwifi.sh
+++ setupwifi.sh
@@ -28,3 +28,37 @@ echo "(sleep 60) &" >> rc.local
 
 
 echo "exit 0" >> rc.local
+
+> testNode.js
+
+
+echo "const http = require('http');" >> testNode.js
+echo "var exec = require('child_process').exec;" >> testNode.js
+echo "var child;" >> testNode.js
+echo "" >> testNode.js
+echo "http.get('http://worldclockapi.com/api/json/utc/now', resp => {" >> testNode.js
+echo "	let data = '';" >> testNode.js
+echo "	resp.on('data', chunk => {" >> testNode.js
+echo "		data += chunk;" >> testNode.js
+echo "	});" >> testNode.js
+echo "" >> testNode.js
+echo "	resp.on('end', () => {" >> testNode.js
+echo "		var date = JSON.parse(data);" >> testNode.js
+echo "		child = exec('date --set '+date.currentDateTime, function(error, stdout, stderr) {" >> testNode.js
+echo "		console.log('stdout: ' + stdout);" >> testNode.js
+echo "		console.log('stderr: ' + stderr);" >> testNode.js
+echo "		if(error !== null) {" >> testNode.js
+echo "			console.log('Execution Error: ' + error)" >> testNode.js
+echo "		}" >> testNode.js
+echo "});" >> testNode.js
+echo "	});" >> testNode.js
+echo "}).on('error', err => {" >> testNode.js
+echo "	console.log('error: ' + err.message);" >> testNode.js
+echo "});" >> testNode.js
+
+
+
+
+
+
+
